410ef500c36b309eec84bbc3deaaa61e55c7b49e
ricardordz13 <ricardo.rdz.trevino@gmail.com>
2024-03-21 11:15:51 +0100

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Ricardo Jorge Rodriguez Trevino on 21/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
