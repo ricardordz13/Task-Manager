7cd6847ceee6c4b311b7cdf94ec6fccd4cde8705
ricardordz13 <ricardo.rdz.trevino@gmail.com>
2024-03-25 11:51:29 +0100

    First Commit

---
+++ TaskManager/Helpers/Date.swift
@@ -0,0 +1,8 @@
+//
+//  Date.swift
+//  TaskManager
+//
+//  Created by Ricardo Jorge Rodriguez Trevino on 23/03/24.
+//
+
+import Foundation
---
+++ TaskManager/Helpers/OffsetKey.swift
@@ -0,0 +1,8 @@
+//
+//  OffsetKey.swift
+//  TaskManager
+//
+//  Created by Ricardo Jorge Rodriguez Trevino on 25/03/24.
+//
+
+import Foundation
