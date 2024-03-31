9ad1d41537310167d369b2b6fea256d8e4f49915
ricardordz13 <ricardo.rdz.trevino@gmail.com>
2024-04-01 00:23:00 +0200

    Sttaic Functionality

---
+++ TaskManager/Models/Assignment.swift
@@ -0,0 +1,8 @@
+//
+//  Assignment.swift
+//  TaskManager
+//
+//  Created by Ricardo Jorge Rodriguez Trevino on 31/03/24.
+//
+
+import Foundation
---
+++ TaskManager/Views/DailyTasksView.swift
@@ -0,0 +1,18 @@
+//
+//  DailyTasksView.swift
+//  TaskManager
+//
+//  Created by Ricardo Jorge Rodriguez Trevino on 27/03/24.
+//
+
+import SwiftUI
+
+struct DailyTasksView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DailyTasksView()
+}
