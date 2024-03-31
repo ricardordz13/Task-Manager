8a327658b7c9d856bdaba16a8e6a92261b94d647
ricardordz13 <ricardo.rdz.trevino@gmail.com>
2024-04-01 01:51:32 +0200

    New Assignment View

---
+++ TaskManager/Views/NewAssignmentView.swift
@@ -0,0 +1,18 @@
+//
+//  NewAssignmentView.swift
+//  TaskManager
+//
+//  Created by Ricardo Jorge Rodriguez Trevino on 01/04/24.
+//
+
+import SwiftUI
+
+struct NewAssignmentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewAssignmentView()
+}
