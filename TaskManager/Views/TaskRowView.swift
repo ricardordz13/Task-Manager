93b58b4672686571b90e068681f86afd382c2b4a
ricardordz13 <ricardo.rdz.trevino@gmail.com>
2024-04-02 18:25:09 +0200

    SwiftData+CloudKit

---
+++ TaskManager/Views/TaskRowView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskRowView.swift
+//  TaskManager
+//
+//  Created by Ricardo Jorge Rodriguez Trevino on 01/04/24.
+//
+
+import SwiftUI
+
+struct TaskRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskRowView()
+}
