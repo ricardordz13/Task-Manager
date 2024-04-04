c634a86ddafbf671a957a7f1e85a6e6fa46a7fd3
ricardordz13 <ricardo.rdz.trevino@gmail.com>
2024-04-04 10:37:21 +0200

    Tasks

---
+++ TaskManager/Helpers/PersistenceController.swift
@@ -0,0 +1,8 @@
+//
+//  PersistenceController.swift
+//  TaskManager
+//
+//  Created by Ricardo Jorge Rodriguez Trevino on 03/04/24.
+//
+
+import Foundation
---
+++ TaskManager/Helpers/SceneDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  SceneDelegate.swift
+//  TaskManager
+//
+//  Created by Ricardo Jorge Rodriguez Trevino on 03/04/24.
+//
+
+import Foundation
